979d855f1d9ab2b5248645c024fcac0e80adec1c
savidude <mysticsavy@gmail.com>
2017-06-06 13:19:32 +0530

    Adding motor functions to raspberryPi

--- raspberryPi-agent/src/client.py
+++ raspberryPi-agent/src/client.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import configparser
 import json
+import RPi.GPIO as IO
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -15,6 +16,16 @@ MQTT_TOPIC = "micData"
 print("MQTT BROKER: ", MQTT_BROKER)
 print ("MQTT PORT: ", MQTT_PORT)
 
+#source: https://circuitdigest.com/microcontroller-projects/raspberry-pi-servo-motor-control
+IO.setwarnings(False)          # do not show any warnings
+IO.setmode (IO.BCM)            # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+IO.setup(19,IO.OUT)            # initialize GPIO19 as an output
+p = IO.PWM(19,50)              # GPIO19 as PWM output, with 50Hz frequency
+p.start(7.5)                   # generate PWM signal with 7.5% duty cycle
+#servo position to 0º =  p.ChangeDutyCycle(2.5)
+#servo position to 180º =  p.ChangeDutyCycle(12.5)
+
+motor_angle = 0
 # Define on_connect event Handler
 def on_connect(mosq, obj, rc):
     #Subscribe to a the Topic
@@ -31,7 +42,11 @@ def on_message(mqttc, userdata, message):
     data = json.loads(msg)
     for item in data["mics"]:
         if item["state"] == 1:
-            print(item["angle"])
+            motor_angle = item["angle"]
+            print("Motor angle: ", motor_angle)
+    duty_cycle_value = 2.5 + (10 / 180) * motor_angle
+    print("Duty cycle value: ", duty_cycle_value)
+    p.ChangeDutyCycle(duty_cycle_value)
 
 # Initiate MQTT Client
 mqttc = mqtt.Client()
